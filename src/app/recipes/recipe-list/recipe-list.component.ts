a569161176bb02d9b7665a8f12b9f0cf75f276c4
Chandra Shekhar <shekharindian099@gmail.com>
2016-11-08 15:10:53 +0530

    add ingredients and dummy recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,12 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[];
+  recipes: Recipe[] = [
+    new Recipe('recipe1','this is first dummy recipe', 'https://s-media-cache-ak0.pinimg.com/564x/a3/10/95/a31095e1d2aac1ff3bf7c4b809dad6a1.jpg', []),
+    new Recipe('recipe2','this is second dummy recipe', 'http://i164.photobucket.com/albums/u8/hemi1hemi/COLOR/COL9-6.jpg', [])
+  ]
+
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('dummy','this is dummy recipe', 'https://s-media-cache-ak0.pinimg.com/564x/a3/10/95/a31095e1d2aac1ff3bf7c4b809dad6a1.jpg');
   constructor() { }
 
   ngOnInit() {
