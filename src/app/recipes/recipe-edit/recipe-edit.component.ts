ab07b4818661d266f8e28a99f4ac3100bfe9dd2f
Chandra Shekhar <shekharindian099@gmail.com>
2016-11-11 14:01:19 +0530

    add and save new recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,7 +2,7 @@ import { RecipesService } from './../recipes.service';
 import { Recipe } from './../recipe';
 import { Ingredients } from './../ingredients';
 import { Subscription } from 'rxjs/Rx';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
@@ -19,7 +19,10 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   private recipe: Recipe;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipesService, private formBuilder: FormBuilder) { }
+  constructor(private route: ActivatedRoute, 
+              private recipeService: RecipesService, 
+              private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
     this.subscription = this.route.params.subscribe(
@@ -66,6 +69,24 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
   }
 
+  onSubmit() {
+    const newRecipe = this.recipeForm.value; 
+    if(this.isNew) {
+      this.recipeService.addRecipe(newRecipe);
+    }
+    else {
+      this.recipeService.editRecipe(this.recipe,newRecipe);
+    }
+    this.navigateBack();
+  }
+
+  onCancel() {
+    this.navigateBack();
+  }
+
+  private navigateBack() {
+    this.router.navigate(['../']);
+  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
